6f014b12407494cc68f41f7442c804995ca4251b
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-07-12 12:38:08 -0400

    Add net/ftp to net_fetcher

--- lib/omnibus/fetchers/net_fetcher.rb
+++ lib/omnibus/fetchers/net_fetcher.rb
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require 'net/http'
 
 module Omnibus
   class UnsupportedURIScheme < ArgumentError
